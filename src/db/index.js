9cde81a279b77bd8fa8d41dfed0711a0d5ee3c29
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-08-11 00:11:53 -0400

    ✍🏽 Update index.js file

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,11 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || "",
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+  user: 'testdd',
+  host: 'localhost',
+  database: 'scheduler_development',
+  password: 'testdd',
+  port: 5432,
 });
 
 client
